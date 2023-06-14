02719ff1840fe2f482e33bb483838df6257774fc
Biswajit swain <biswajitswain820@gmail.com>
2023-06-14 10:16:15 +0530

    third commit

--- src/shared/VideoLength.jsx
+++ src/shared/VideoLength.jsx
@@ -4,7 +4,7 @@ import moment from "moment/moment";
 const VideoLength = ({ time }) => {
   const vdoLngthInSec = moment().startOf("day").seconds(time).format("H:mm:ss");
   return (
-    <div className="absolute bottom-1 right-1 bg-black py-1 px-2 text-white text-xs rounded-md">
+    <div className="absolute bottom-1 right-2 bg-black py-1 px-2 text-white text-xs rounded-md">
       {vdoLngthInSec}
     </div>
   );
